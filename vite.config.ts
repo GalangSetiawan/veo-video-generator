6b82176070d463d2c6272e60febc13b11664786e
GalangSetiawan <galang.setiawan.syn@gmail.com>
2025-09-09 23:29:27 +0700

    ss

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
+    const GEMINI_API_KEY = 'tokenku'
     return {
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        'process.env.API_KEY': JSON.stringify(GEMINI_API_KEY),
+        'process.env.GEMINI_API_KEY': JSON.stringify(ssGEMINI_API_KEY)
       },
       resolve: {
         alias: {
